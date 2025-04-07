9e61d7866b05b2bf748abbe4dc0f57e3f08632f5
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-07 09:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-//complete this code
-class Person {}
+// Person class
+class Person {
+    constructor(name, age) {
+        this._name = name; // Initialize name property
+        this._age = age;   // Initialize age property
+    }
 
-class Student extends Person {}
+    // Getter for name
+    get name() {
+        return this._name; // Return the name
+    }
 
-class Teacher extends Person {}
+    // Getter for age
+    get age() {
+        return this._age; // Return the age
+    }
+
+    // Setter for age
+    set age(age) {
+        this._age = age; // Update the age
+    }
+}
+
+// Student class that inherits from Person
+class Student extends Person {
+    // Method for studying
+    study() {
+        console.log(`${this.name} is studying`); // Log the study message
+    }
+}
+
+// Teacher class that inherits from Person
+class Teacher extends Person {
+    // Method for teaching
+    teach() {
+        console.log(`${this.name} is teaching`); // Log the teach message
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
